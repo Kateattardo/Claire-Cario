e05e7466b6ce23ca5b3a5169f7aac233dd4c5c1c
Kateattardo <youremail@domain.com>
2024-01-19 15:48:33 -0500

    image addedto home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,46 @@
 import React from "react";
+import Otis from "../assets/Otis.jpg";
 
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#faebd7]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#556b2f]">
-          Claire F. Cario
-        </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#006400]">
-          MA, CPDT-KA, FFCT
-        </h2>
+        <div className="flex items-center justify-center sm:justify-start">
+          {/* Headings */}
+          <div>
+            <h1 className="text-4xl sm:text-7xl font-bold text-[#556b2f]">
+              Claire F. Cario
+            </h1>
+            <h2 className="text-4xl sm:text-7xl font-bold text-[#006400]">
+              MA, CPDT-KA, FFCT
+            </h2>
+          </div>
+          {/* Circular Image */}
+          <img
+            src={Otis}
+            alt="Home"
+            className="rounded-full w-25 h-25 ml-4 object-cover"
+          />
+        </div>
       </div>
     </div>
   );
 };
+
+//   return (
+//     <div name="home" className="w-full h-screen bg-[#faebd7]">
+//       {/* Container */}
+//       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+//         <h1 className="text-4xl sm:text-7xl font-bold text-[#556b2f]">
+//           Claire F. Cario
+//         </h1>
+//         <h2 className="text-4xl sm:text-7xl font-bold text-[#006400]">
+//           MA, CPDT-KA, FFCT
+//         </h2>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default Home;
