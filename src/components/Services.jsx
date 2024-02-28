5407e2aa165c3e0414c7e018a1e6a4de8d332ae2
Kateattardo <youremail@domain.com>
2024-02-28 14:46:26 -0500

    services updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -14,33 +14,38 @@ const Services = () => {
         <div className="max-w-4xl mx-auto px-4 w-full grid md:grid-cols-2 gap-8">
           {/* In Home Rates Column */}
           <div className="text-center sm:text-left">
-            <h1>In Home Initial Consultation (1 hour)</h1>
-            <h2>
+            <h1 className="text-2xl pb-4">
+              In Home Initial Consultation (1 hour)
+            </h1>
+            <h2 className="text-l pb-4">
               Help figure out and address behavior concerns immediately in the
               home and out in public. Assess and tweak your current managment
               tools and protocols. Educate you and your dog on how to use
               reinforcement-based methods in an impactful way. Create a
               customized training plan to address your goals.
             </h2>
-            <h1>Follow-up Sessions (1 hour)</h1>
-            <h1>3 Package Sessions</h1>
-            <h1>5 Package Sessions</h1>
+            <h1 className="text-2xl pb-4">Follow-up Sessions (1 hour)</h1>
+            <h1 className="text-2xl pb-4">3 Package Sessions</h1>
+            <h1 className="text-2xl">5 Package Sessions</h1>
           </div>
 
           {/* Virtual Rates Column */}
           <div className="text-center sm:text-right">
-            <h1>Virtual Initial Consultation (1 hour)</h1>
-            <h2>
+            <h1 className="text-2xl pb-4">
+              Virtual Initial Consultation (1 hour)
+            </h1>
+            <h2 className="text-l pb-4">
               Virtual training can be scheduled during the day or evening. Great
               supplement to private lessons ect ect.
             </h2>
-            <h1>Follow-up Sessions (1 hour)</h1>
-            <h1>3 Package Sessions</h1>
-            <h1>5 Package Sessions</h1>
+            <h1 className="text-2xl pb-4">Follow-up Sessions (1 hour)</h1>
+            <h1 className="text-2xl pb-4">3 Package Sessions</h1>
+            <h1 className="text-2xl">5 Package Sessions</h1>
           </div>
+
           <div className="max-w-4xl mx-auto px-4 w-full text-center pb-8">
-            <p className="text-2xl sm:text-3xl font-bold">
-              A follow up report is included with every session.
+            <p className="text-2xl sm:text-3xl font-bold inline border-b-4 border-white">
+              A follow up report is included with every session
             </p>
           </div>
         </div>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -26,7 +26,7 @@ const Testimonials = () => {
       <div className="flex flex-col justify-center items-center w-full">
         {/* <div className="max-w-4xl mx-auto px-4 w-full grid sm:grid-cols-2 gap-8"> */}
         <div className="max-w-4xl mx-auto px-4 w-full grid grid-cols-1 gap-8">
-          <div className="text-center sm:text-right pb-8">
+          <div className="text-center  pb-8">
             <p className="text-2xl sm:text-3xl font-bold inline border-b-4 border-white">
               Testimonials
             </p>
