55166cb654b2d23f094d2e2f383580bf98a28b29
Kateattardo <youremail@domain.com>
2024-03-12 14:27:36 -0400

    submit form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
     >
       <form
         method="POST"
-        action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c"
+        action="https://getform.io/f/zazookmb"
         className="flex flex-col max-w-[600px] w-full"
       >
         <div className="pb-8">
@@ -19,6 +19,7 @@ const Contact = () => {
             Submit the form below to get info about training.
           </p>
         </div>
+
         <input
           className="bg-white p-2"
           type="text"
